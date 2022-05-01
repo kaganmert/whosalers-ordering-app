bbae5b0125ffb404b322d243629ad1b846d872f4
Kagan <kaganmert@hotmail.com.tr>
2022-05-02 00:07:38 +0300

    Add required packages Fixes #1

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
+
+import { store } from "./app/store";
+import Routes from "./routes";
+
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes />
+        <Toaster />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from "react";
+
+const Login: React.FC = (): ReactElement => {
+  return <h2>Login Page</h2>;
+};
+
+export default Login;
